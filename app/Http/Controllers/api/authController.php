f33954a59d0f3aa850c5a6db76adcacbf6d5727a
Softlabltd.com <freelancernishad123@gmail.com>
2023-03-25 17:24:57 +0600

    update

--- app/Http/Controllers/api/authController.php
+++ app/Http/Controllers/api/authController.php
@@ -94,12 +94,14 @@ class authController extends Controller
 
         $data = [];
         $data = $r->except(['password','password_confirmation','ref_by']);
+
         $ref_by=  $r->ref_by;
-        if($ref_by==16346){
-            $data['ref_by'] = 17597;
-        }else{
-            $data['ref_by'] = $ref_by;
-        }
+        // if($ref_by==16346){
+        //     $data['ref_by'] = 17597;
+        // }else{
+        //     $data['ref_by'] = $ref_by;
+        // }
+        $data['ref_by'] = $ref_by;
 
         $data['password'] = hash::make($r->password);
         $data['username'] =$username;
@@ -110,7 +112,7 @@ class authController extends Controller
         $data['plan_id'] =planId($bonus);
         $data['ip'] = $clientIP;
         $data['status'] ='active';
-   
+
         $validator = Validator::make($r->all(), [
             'name' => 'required|min:4',
             // 'email' => 'required|email|unique:users',
